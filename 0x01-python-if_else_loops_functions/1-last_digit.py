91fc396741a419d901fbae8875c3d13e18c62f16
MandlaC1008 <mandlashandu15@gmail.com>
2023-12-05 03:25:40 -0800

    Task

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+lastigit = abs(number) % 10
+if lastdigit < 0:
+    lastdigit = -(lastdigit)
+thestring = "lastdit of {} is {}".format(number,lastdigit)
+if lastdigit > 5:
+    print(f"{thestring} is greater than 5")
+elif lastdigit == 0:
+    print(f"{the string} is 0")
